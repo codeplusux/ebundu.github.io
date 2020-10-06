73f98eb13e1ba398e3bef9f7f40ea00bd3f8123b
codeplusux <dev@pablovivanco.com>
2020-10-06 18:10:29 -0300

    Updates

---
+++ precache-manifest.f484af6c47cc6f961b02ae3605c3d910.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f464185dc77ec2705444c2b734ae55f7",
+    "url": "/ebundu-landing/index.html"
+  },
+  {
+    "revision": "3ad12ba912b8ab1b8ad1",
+    "url": "/ebundu-landing/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e5199528bc9ea2f4cc80",
+    "url": "/ebundu-landing/static/css/main.1d6646c4.chunk.css"
+  },
+  {
+    "revision": "3ad12ba912b8ab1b8ad1",
+    "url": "/ebundu-landing/static/js/2.44660e85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ebundu-landing/static/js/2.44660e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5199528bc9ea2f4cc80",
+    "url": "/ebundu-landing/static/js/main.bc59c2c3.chunk.js"
+  },
+  {
+    "revision": "da3a0cc84801acf2fc3f",
+    "url": "/ebundu-landing/static/js/runtime-main.ff8e13e8.js"
+  },
+  {
+    "revision": "93425ab0adb678df874c999fd72fc446",
+    "url": "/ebundu-landing/static/media/androidBadge.93425ab0.png"
+  },
+  {
+    "revision": "e15e09524b7af921d6ebe90f2b8235d0",
+    "url": "/ebundu-landing/static/media/appleBadge.e15e0952.png"
+  }
+]);
